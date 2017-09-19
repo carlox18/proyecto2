c5a99386b79731bbd6ef5279d0ce0fa9a415db89
Juan David Guzmán Sarmiento <jdguzmans@hotmail.com>
2017-09-18 20:23:30 -0500

    Strings deben usar singlequote

--- front/src/index.js
+++ front/src/index.js
@@ -5,7 +5,7 @@ import registerServiceWorker from './registerServiceWorker';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import App from "./components/App.js";
+import App from './components/App.js';
 
 
 import { BrowserRouter} from 'react-router-dom';
